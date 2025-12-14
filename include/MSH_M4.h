cb78c219cfccf7b76895634f4a482a811440dbac
Övünç Aygün <ovunc.aygun@gmail.com>
2025-12-14 13:14:20 +0300

    Module 4 initialization

---
+++ include/MSH_M4.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "IStateManager.h"
+
+class MSH_M4 {
+public:
+    MSH_M4();
+    ~MSH_M4();
+
+    IStateManager* getIStateManager();
+};
---
+++ src/MSH_M4.cpp
@@ -0,0 +1,8 @@
+#include "MSH_M4.h"
+
+MSH_M4::MSH_M4() = default;
+MSH_M4::~MSH_M4() = default;
+
+IStateManager* MSH_M4::getIStateManager() {
+    return NULL;
+}
